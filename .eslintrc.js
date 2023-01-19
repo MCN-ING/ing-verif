fec0fba7f964d126f2fef3a6378b57dcb41c9b06
Jean-Christophe <Jean-Christophe.Drouin@mcn.gouv.qc.ca>
2023-01-19 08:48:05 -0500

    Change eslint settings
    Signed-off-by: Jean-Christophe <Jean-Christophe.Drouin@mcn.gouv.qc.ca>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,70 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  extends: [
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
   plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': 'error',
+    // Because of early development, we only warn on ts-ignore. In future we want to move to error
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    // Aries protocol defines attributes with snake case.
+    '@typescript-eslint/camelcase': 'off',
+    // Type is enforced by callers. Not entirely, but it's good enough.
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['type', ['builtin', 'external'], 'parent', 'sibling', 'index'],
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    'import/no-cycle': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: false,
+      },
+    ],
+  },
+  globals: {
+    require: true,
+  },
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['*.test.*'],
       rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
+        'no-console': 'off',
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+    {
+      files: ['aries-components/**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-namespace': 'off',
+      },
+    },
+    {
+      files: ['.eslintrc.js', '*.config.js'],
+      env: {
+        node: true,
       },
     },
   ],
-};
+}
