a5bf9afe06cdff8c56650b97c2fbda3dd7d9e198
Jean-Christophe <Jean-Christophe.Drouin@mcn.gouv.qc.ca>
2023-02-03 14:30:00 -0500

    Home page 1.0
    Signed-off-by: Jean-Christophe <Jean-Christophe.Drouin@mcn.gouv.qc.ca>

--- src/localization/fr/index.ts
+++ src/localization/fr/index.ts
@@ -1,7 +1,26 @@
 const translation = {
-  welcome: 'Bienvenue',
+  Home: {
+    Title: 'Vérifier une identité',
+    Slider: {
+      Title1: 'Vérifier ...',
+      Body1: "Utilisez le code QR pour vérifier l'identité d'une personne.",
+      Title2: 'Changer le schéma de vérification',
+      Body2: 'pour vérifier l’identité d’une personne.',
+      Title3: 'Vérification Bluetooth',
+      Body3: 'Utilisez Bluetooth pour vérifier l’identité d’une personne.',
+    },
+  },
+  ActivityLog: {
+    EmptyTitle: 'Activité',
+    Title: 'Nouvelles activités',
+    EmptyBody: "Vous êtes à jour, il n'y a rien de nouveau.",
+  },
   sections: {
     schema: 'Schémas',
+  },
+  Screens: {
+    Home: 'Accueil',
+    QRCode: 'Connexion',
   },
 }
 
--- src/navigators/RootStack.tsx
+++ src/navigators/RootStack.tsx
@@ -1,15 +1,44 @@
 import {createStackNavigator} from '@react-navigation/stack'
 import React from 'react'
+import {useTranslation} from 'react-i18next'
 
+import {useTheme} from '../contexts/theme'
 import {Home, Settings, QRCodeScreen, ValidationResult} from '../screens'
 
 const RootStack = () => {
+  const {t} = useTranslation()
   const Stack = createStackNavigator()
+  const {ColorPallet} = useTheme()
 
   return (
-    <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="QRCode" component={QRCodeScreen} />
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: ColorPallet.primary,
+        },
+      }}>
+      <Stack.Screen
+        name="Home"
+        options={{
+          title: t('Screens.Home') || '',
+          headerTintColor: ColorPallet.white,
+          headerShown: true,
+          gestureEnabled: false,
+          headerLeft: () => false,
+        }}
+        component={Home}
+      />
+      <Stack.Screen
+        name="QRCode"
+        component={QRCodeScreen}
+        options={{
+          title: t('Screens.QRCode') || '',
+          headerTintColor: ColorPallet.white,
+          headerShown: true,
+          gestureEnabled: true,
+        }}
+      />
       <Stack.Screen name="Settings" component={Settings} />
       <Stack.Screen name="ValidationResult" component={ValidationResult} options={{presentation: 'modal'}} />
     </Stack.Navigator>
