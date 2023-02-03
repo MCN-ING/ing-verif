a38ac4c5a1b6180c101cd4d6fc868a56339c8c79
agueye84 <amadagueye@gmail.com>
2023-02-03 14:38:45 -0500

    deploy ios

--- scripts/makepp.sh
+++ scripts/makepp.sh
@@ -6,8 +6,8 @@ PP_DIR=~/Library/MobileDevice/Provisioning\ Profiles
 echo ">> Build Provisioning Profile... 🤞"
 echo ">> Provisioning Profile Home = ${PP_DIR}"
 
-UUID=$(/usr/libexec/plistbuddy -c Print:UUID /dev/stdin <<< `echo "${PROVISIONING_PROFILE}" | base64 -d | security cms -D`)
-echo "${PROVISIONING_PROFILE}" | base64 -d >${UUID}.mobileprovision
+UUID=$(/usr/libexec/plistbuddy -c Print:UUID /dev/stdin <<< `echo "${BUILD_PROVISION_PROFILE_BASE64}" | base64 -d | security cms -D`)
+echo "${BUILD_PROVISION_PROFILE_BASE64}" | base64 -d >${UUID}.mobileprovision
 md5 "${UUID}.mobileprovision"
 mkdir -p "${PP_DIR}"
 cp ${UUID}.mobileprovision "${PP_DIR}/"
