6aaee3b0f3d7808d7608635156b7abc440e9a572
agueye84 <amadagueye@gmail.com>
2023-02-28 14:18:29 -0500

    Améliorer le texte en anglais
    Signed-off-by: agueye84 <amadagueye@gmail.com>

--- src/localization/en/index.ts
+++ src/localization/en/index.ts
@@ -11,12 +11,10 @@ const translation = {
   Home: {
     Title: 'Verify an identity',
     Slider: {
-      Title1: 'Verify ...',
+      Title1: 'Select a request',
       Body1: 'Use the QR code to verify the identity of a person.',
       Title2: 'Change verification schema',
       Body2: 'Change the verification schema to verify the identity of a person.',
-      Title3: 'Bluetooth verification',
-      Body3: 'Use Bluetooth to verify the identity of a person.',
     },
   },
   QRCode: {
