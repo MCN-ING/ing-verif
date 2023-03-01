9abdcfb3b9d1006ef8e1375cc600f51898812b95
agueye84 <amadagueye@gmail.com>
2023-03-01 18:20:14 -0500

    centrer header
    Signed-off-by: agueye84 <amadagueye@gmail.com>

--- src/navigators/RootStack.tsx
+++ src/navigators/RootStack.tsx
@@ -39,6 +39,8 @@ const RootStack = () => {
         headerStyle: {
           backgroundColor: ColorPallet.primary,
         },
+        headerTitleAlign: 'center',
+
       }}>
       <Stack.Screen
         name="Splash"
