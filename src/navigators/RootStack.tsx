5c86f2a62547fcb380ee7d7891b1d7875e861497
agueye84 <adama.gueye-ext@mcn.gouv.qc.ca>
2023-03-02 09:59:37 -0500

    Centrer le header
    Signed-off-by: agueye84 <adama.gueye-ext@mcn.gouv.qc.ca>

--- src/navigators/RootStack.tsx
+++ src/navigators/RootStack.tsx
@@ -39,6 +39,7 @@ const RootStack = () => {
         headerStyle: {
           backgroundColor: ColorPallet.primary,
         },
+        headerTitleAlign: 'center',
       }}>
       <Stack.Screen
         name="Splash"
